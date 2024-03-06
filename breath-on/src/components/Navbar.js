f244ce42cce163e856b9453bbdad6c78cbb0ee33
ilaria <ilariaferrauti@gmail.com>
2024-03-06 16:21:44 +0100

    setto le variabili e inizio componente navbar

--- breath-on/src/components/Navbar.js
+++ breath-on/src/components/Navbar.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { TbMoonFilled } from "react-icons/tb";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className="fixed border-gray-500 border-opacity-50 border-2 top-0 left-0 right-0">
+      <div className="flex container border-2 border-black justify-around items-center">
+        <img src="./assets/images/logo.svg" />
+        <TbMoonFilled className="h-7 w-7" />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- breath-on/tailwind.config.js
+++ breath-on/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5EA9BE",
+        secondary: "#FFDDD3",
+        textColor: "#222222",
+      },
+      fontFamily: {
+        "petit-formal-script": ["Petit Formal Script", "cursive"],
+        petrona: ["Petrona", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
