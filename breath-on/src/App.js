01528588de5f933ef856354f2cefe56b81fd80d4
ilaria <ilariaferrauti@gmail.com>
2024-03-06 16:54:54 +0100

    setto le route per la home

--- breath-on/src/App.js
+++ breath-on/src/App.js
@@ -1,11 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- breath-on/src/components/Navbar.js
+++ breath-on/src/components/Navbar.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { TbMoonFilled } from "react-icons/tb";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleDarkTheme = () => {
+    setIsDark(!isDark);
+  };
   return (
-    <nav className="fixed border-gray-500 border-opacity-50 border-2 top-0 left-0 right-0">
-      <div className="flex container border-2 border-black justify-around items-center">
-        <img src="./assets/images/logo.svg" />
-        <TbMoonFilled className="h-7 w-7" />
+    <nav className="fixed flex border-gray-500 border-opacity-50 border-2 top-0 left-0 right-0 bg-[#FFD6CB]">
+      <div className="flex container m-auto border-2 border-black justify-between items-center py-7 px-5">
+        <Link to="/">
+          <img src="./assets/images/logo.svg" alt="Logo" />
+        </Link>
+        <button onClick={toggleDarkTheme}>
+          <TbMoonFilled className="h-8 w-8 text-primary" />
+        </button>
       </div>
     </nav>
   );
