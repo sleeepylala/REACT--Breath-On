e7ad4ffe2f069fc3497d20aea19a6738e3370887
ilaria <ilariaferrauti@gmail.com>
2024-03-25 16:09:40 +0100

    refactor Home

--- breath-on/src/components/Home.js
+++ breath-on/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { FaArrowRight } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import hero from "../assets/images/hero-img.svg";
@@ -8,23 +8,22 @@ import { useTheme } from "../context/ThemeContext";
 const Home = () => {
   const { darkMode } = useTheme();
 
-  useEffect(() => {}, []);
-
   return (
     <section className="py-16 section-home">
       <div
-        className={`grid grid-cols-1 sm:grid-cols-12 border-2 border-blue-900 ${
-          darkMode ? "bg-darkPrimary" : "bg-secondary"
+        className={`grid grid-cols-1 sm:grid-cols-12 border-2 ${
+          darkMode ? "border-blue-900 bg-darkPrimary" : "bg-secondary"
         }`}
       >
-        <div className="col-span-6 flex flex-col justify-center items-center ">
+        <div className="col-span-6 flex flex-col justify-center items-center">
           <h1
-            className={`${
+            className={`text-center font-petit-formal-script text-3xl md:text-4xl lg:text-6xl ${
               darkMode ? "text-darkTextColor2" : "text-textColor"
-            }  text-center font-petit-formal-script text-3xl md:text-4xl lg:text-6xl`}
+            }`}
           >
             Take a breath <br /> and ease your mind
           </h1>
+
           <Link
             to="/timer"
             className="mt-16 flex items-center text-textColor font-petrona text-1xl lg:text-2xl sm:text-xl border rounded-xl bg-white px-6 py-2"
@@ -35,15 +34,10 @@ const Home = () => {
         </div>
 
         <div className="col-span-6">
-          {darkMode ? (
-            <img src={heroDark} alt="hero" />
-          ) : (
-            <img src={hero} alt="hero" />
-          )}
+          <img src={darkMode ? heroDark : hero} alt="hero" />
         </div>
       </div>
     </section>
   );
 };
-
 export default Home;
