4ed55b3839a1c1c0124990fb1121ad64b1c4895a
ilaria <ilariaferrauti@gmail.com>
2024-03-25 15:32:49 +0100

    salvo nel local storage la darkmode

--- breath-on/src/context/ThemeContext.js
+++ breath-on/src/context/ThemeContext.js
@@ -1,11 +1,20 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const ThemeContext = createContext();
 
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    // Controlla se c'è un valore salvato in localStorage al caricamento dell'applicazione
+    const savedDarkMode = localStorage.getItem("darkMode");
+    return savedDarkMode ? JSON.parse(savedDarkMode) : false;
+  });
+
+  // Effetto per salvare lo stato del tema in localStorage ogni volta che cambia
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
 
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
